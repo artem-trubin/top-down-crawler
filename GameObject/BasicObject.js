fb1abcfcd44e2f72bdc2384d5cc8b10086954d8b
artem-trubin <17trubin@gmail.com>
2022-12-06 18:04:43 +0200

    Added BasicObject

---
+++ GameObject/BasicObject.js
@@ -0,0 +1,21 @@
+export class BasicObject {
+  constructor(x, y, w, h, name, updateFn, drawFn) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.name = name;
+    this.updateFn = updateFn;
+    this.drawFn = drawFn;
+  }
+
+  get top() { return this.y }
+  get bottom() { return this.y + this.height }
+  get left() { return this.x }
+  get right() { return this.x + this.width }
+
+  set top(v) { this.y = v }
+  set bottom(v) { this.y = v - this.height }
+  set left(v) { this.x = v }
+  set right(v) { this.x = v - this.width }
+}
